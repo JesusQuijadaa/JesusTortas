3639380653cf1d3f99ed75f14146bb16119fe647
Jesus Quijada <jesusquijada290@gmail.com>
2023-09-24 21:26:09 -0300

    ProFinal

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-async function cargaFetch() {
-    const urlLocal = "tortas.json";
-    const resp = await fetch(urlLocal);
-    const data = await resp.json();
-    let tortas = data.tortas;
-    insertarProductoEnHtml(tortas);
-}
-cargaFetch()
+fetch(`tortas.json`)
+    .then(response => response.json())
+    .then(data => {
+        let tortas = data.tortas;
+        insertarProductoEnHtml(tortas);
+    }) 
 
 
 
@@ -25,7 +23,6 @@ class Torta {
     quitarCarrito() {
         this.cantidad--;
     }
-
 
     precioFinalTotal() {
         this.precioTotal = this.precio * this.cantidad;
@@ -56,7 +53,7 @@ function insertarProductoEnHtml(tortas) {
                     <p class="card-text precio">$${torta.precio}</p>
                     <p>Unidades disponibles: ${torta.stock == 0 ? "NO HAY STOCK" :  torta.stock}</p>
                     <div class="text-center">
-                        <button id="Agregar${torta.id}" type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                    <button id="Agregar${torta.id}" type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                       </svg> Añadir al carrito
                       </button>
@@ -85,8 +82,8 @@ function cargarCarrito() {
             <td>${items.nombre}</td>
             <td id="${items.id}">${items.cantidad}</td>
             <td >${items.precio}</td>
-             
-        </tr>`
+
+            </tr>`
         })
         //se actualiza subtotal
     document.getElementById("subtotal").innerText = `Subtotal: $${subtotal()}`;
@@ -116,7 +113,7 @@ function agregarCarrito(torta) {
         </tr>`)
         localStorage.setItem("carrito", JSON.stringify(carrito));
         Swal.fire({
-                title: `${torta.nombre}`,
+            title: `${torta.nombre}`,
                 text: `Agregaste ${torta.nombre} al carrito`,
                 imageUrl: `${torta.img}`,
                 imageHeight: 200,
@@ -144,8 +141,6 @@ function agregarCarrito(torta) {
         document.getElementById("subtotal").innerText = `Subtotal: $${subtotal()}`;
     }
 }
-
-
 
 
 
@@ -205,3 +200,5 @@ function vaciarCarrito() {
         icon: 'success',
     })
 }
+
+ 
